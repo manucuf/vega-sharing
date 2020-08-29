8cc9caaf0241523147cda715ee6f9254d75075b5
Emanuele Cufino <emanuele.cufino@outlook.com>
2020-08-29 19:41:49 +0200

    add ConfigService and .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: ['.env.development.local', '.env.development'],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
