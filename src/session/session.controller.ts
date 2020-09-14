3855b89db386c5861d4b8a5378209b8544e2bea9
Emanuele Cufino <emanuele.cufino@outlook.com>
2020-09-14 12:56:03 +0200

    feature(users): implement getUsersFromSubstring, feature(session): add jwt guard

--- src/session/session.controller.ts
+++ src/session/session.controller.ts
@@ -1,19 +1,31 @@
-import { InternalServerErrorException, NotFoundException, BadRequestException, Controller, Body, Post, Get, Req, Query } from '@nestjs/common';
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Get,
+    InternalServerErrorException,
+    NotFoundException,
+    Post,
+    Query,
+    Req,
+    UseGuards,
+} from '@nestjs/common';
 import { SessionDto } from './dto/SessionDto';
 import { SessionService } from './session.service';
 import { SessionError } from './SessionError';
-import {Session} from './schema/session.schema';
+import { Session } from './schema/session.schema';
+import { JwtAuthGuard } from '../authentication/jwt-auth.guard';
 
 @Controller('sessions')
 export class SessionController { 
 
     constructor(private readonly sessionService: SessionService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Post('create')
     async create(@Body() body: SessionDto): Promise<Session> {
         try {
-            const createdSession = await this.sessionService.create(body.name, body.description, body.creatorId, body.roomId);
-            return createdSession;
+            return await this.sessionService.create(body.name, body.description, body.creatorId, body.roomId);
         } catch (exception) {
             if (exception instanceof SessionError) {
                 throw new BadRequestException({
@@ -26,6 +38,7 @@ export class SessionController {
         }
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/')
     async getSessionsByRoomId(@Req() res, @Query() params): Promise< Pick<Session, "_id" | "name" | "description" | "creator" | "room">[] | undefined> {
         try {
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Query, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserPayloadDto } from './dto/UserPayloadDto';
+import { JwtAuthGuard } from '../authentication/jwt-auth.guard';
+import { User } from './schema/user.schema';
 
-@Controller('user')
+@Controller('users')
+
+
 export class UserController {
 
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/')
+  async getUsersFromSubstring(@Req() res, @Query() params): Promise< Pick<User, "_id" | "email" | "name" | "lastname">[] | undefined> {
+      try {
+        return await this.userService.getUsersFromSubstring(params.q);
+      } catch (ex) {
+        throw new NotFoundException();
+      }
+    }
 
 }
