b6278b9d07714f9887cd641132687a28900b8587
manucuf <emanuele.cufino@outlook.com>
2020-09-21 20:04:30 +0200

    Update UserPayloadDto.ts

--- src/authentication/dto/UserPayloadDto.ts
+++ src/authentication/dto/UserPayloadDto.ts
@@ -1,27 +1,5 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
-<<<<<<< HEAD:src/authentication/dto/UserPayloadDto.ts
-=======
-export class LoginPayloadDto {
-  email: string;
-  password: string;
-}
-
-export class SignupPayloadDto {
-  email: string;
-  username: string;
-  password: string;
-  name: string;
-  lastname: string;
-}
-
-
-export class UserPayloadResponseDto {
-  user: SafeUser;
-  token: UserToken;
-}
-
->>>>>>> master:src/authentication/dto/AuthenticationDto.ts
 export class UserPayloadDto {
   @IsNotEmpty()
   name: string;
